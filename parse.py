8cee09e89ee3d62c1eda8a3b61162fc7008c3aad
saarthak24 <saarthaksethi2008@gmail.com>
2017-09-01 21:58:36 -0400

    Updated

--- parse.py
+++ parse.py
@@ -1,3 +1,22 @@
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(open(""))
+import requests
+
+url = input("Enter a website to extract the URL's from: ")
+#https://gomovies.sc/tv/game-of-thrones-season-1-free-full-watch-online/watching/?ep=65&sv=8
+
+r  = requests.get(url)
+
+data = r.text
+
+soup = BeautifulSoup(data, "html.parser")
+
+epFrames = soup.find("div", {"id": "list-eps"})
+
+#print(epFrames)
+
+accessToken = soup.find("a", {"title": "Episode 10: Fire and Blood"}).get("data-drive")
+
+accessToken = "https://play.gomovies.sc/8/" + accessToken
+
+print(accessToken)
