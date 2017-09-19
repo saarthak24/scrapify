eb2744c065f611205cb92b0032aab4611435164e
Rahul Rajan <rahul.rajanus@gmail.com>
2017-09-18 20:22:01 -0400

    basic download

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import send_file
-from flask import render_template, redirect
+from flask import render_template, redirect, jsonify
+
 def root_dir():  # pragma: no cover
     return os.path.abspath(os.path.dirname(__file__))
 @app.route('/')
@@ -12,10 +13,5 @@ def get_downloads():
 	return app.send_static_file('download.html')
 @app.route('/download')
 def return_files_tut():
-	try:
-		#return send_file('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', attachment_filename='google.png')
-		return redirect('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')
-	except Exception as e:
-		return str(e)
-
-	
+    res = {'download' : 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'}
+    return jsonify(res)
